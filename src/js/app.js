43bacce96af7109faf45146661e55a5d214ec451
Alex <qprquo@gmail.com>
2020-02-21 00:13:10 +0300

    Add clients-slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
+import "slick-carousel";
 
-$(()=> {
+$(() => {
   const body = $(document.body);
   $(".b-header__hamburger").click((e) => {
     body.addClass("mobile-menu-is-shown");
@@ -9,4 +10,34 @@ $(()=> {
   $(".b-mobile-menu__close").click((e) => {
     body.removeClass("mobile-menu-is-shown");
   });
+
+  $(".b-clients-slider").slick({
+    slidesToScroll: 1,
+    slidesToShow: 2,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    mobileFirst: true,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 5,
+        }
+      },
+    ]
+  })
 })
